9bd54174ffa7da5c8369b0c1b10467ad55568b81
guychien <guychienll@gmail.com>
2020-10-08 10:15:51 +0800

    Updates

---
+++ precache-manifest.a916b9778603cdf254082fdc19faaaed.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3935ce037efcbba0a21addf5004d7eec",
+    "url": "/guychienll.github.io/todolist/index.html"
+  },
+  {
+    "revision": "4ef1d8efee2433b4a29e",
+    "url": "/guychienll.github.io/todolist/static/js/2.216256a0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/guychienll.github.io/todolist/static/js/2.216256a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "384eefee601b3af2e51c",
+    "url": "/guychienll.github.io/todolist/static/js/main.ec5b6a2c.chunk.js"
+  },
+  {
+    "revision": "cce98748c3604bad8e42",
+    "url": "/guychienll.github.io/todolist/static/js/runtime-main.932f3186.js"
+  }
+]);
